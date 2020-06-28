4e4651fda3fcdb1f4cb698e4d50a93f6c3a96d36
sundar-cloud <sundarbala2589@gmail.com>
2020-06-28 08:12:35 +0530

    added msg
    added the script for project

---
+++ shell_new.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+
+echo "Script has started"
+logger='/home/hduser/sundar/test/test/log'
+
+dir1='/home/hduser/sundar/test/test/dir1'
+dir2='/home/hduser/sundar/test/test/dir1/dir2'
+dir3='/home/hduser/sundar/test/test/dir1/dir3'
+file2='/home/hduser/sundar/test/test/dir1/dir2/file2'
+file3='/home/hduser/sundar/test/test/dir1/dir3/file3'
+
+echo "Checking for directory1 is present" >$logger
+
+if [ -d $dir1 ]; then
+
+    echo "directory1 is exists" >>$logger
+    
+  else
+    
+    echo "directory1 is not exists" >>$logger
+    
+    mkdir $dir1
+    echo "directory1 created newly" >>$logger
+	
+fi
+
+
+
+if [ -d $dir2 ]; then
+
+    echo "directory2 is exists" >>$logger
+    
+	   if [ -f $file2 ]; then 
+	      echo " file2 is exists" >> $logger
+	       rm $file2
+		   test=$?
+		     if [ $test -eq 1 ]; then
+			  echo "failure in remove the file2" >> $logger
+			  exit 1
+			  else
+			    touch $file2
+				fi
+		 else
+          touch $file2		 
+		   fi 
+  else
+    
+    echo "directory2 is not exists" >>$logger
+    
+    mkdir $dir2
+    echo "directory2 created newly" >>$logger
+fi
+
+
+
+if [ -d $dir3 ]; then
+
+    echo "directory3 is exists" >>$logger
+    
+	
+	if [ -f $file3 ]; then 
+	      echo " file3 is exists" >> $logger
+	       rm $file3
+		   test=$?
+		     if [ $test -eq 1 ]; then
+			  echo "failure in remove the file2" >> $logger
+			  exit 1
+			  else
+			    touch $file3
+				fi
+		 else
+          touch $file3		 
+		   fi 
+	
+	
+	
+  else
+    
+    echo "directory3 is not exists" >>$logger
+    
+    mkdir $dir3
+    echo "directory created newly" >>$logger
+fi
+
+
+
+
+
---
+++ source_to_destination.sh
@@ -0,0 +1,85 @@
+
+
+echo " script has starte"
+source_path=/home/hduser/test/source_dir
+log_file=/home/hduser/test/log_file.txt
+emp_file=/home/hduser/test/source_dir/employees.txt
+targ_path=/home/hduser/test/destination_dir
+targ_file=/home/hduser/test/destination_dir/employees.txt
+salary_file=/home/hduser/test/salary.txt
+
+if [ -d $source_path ]; then
+
+echo " Soruce_path is exists " > $log_file
+    
+
+else
+echo " Source_path is not exists so script is terminating" > $log_file
+exit 1 
+fi
+
+
+if [ -f $emp_file ]; then
+
+echo " file is exists in the source path" >> $log_file
+
+else
+
+echo " employees file does not exists so script is terminating " >> $log_file
+
+exit
+
+fi 
+
+
+if [ -d $targ_path ]; then 
+
+echo "Target dir is exists" >> $log_file
+
+else
+
+
+echo " targer dir does not exists" >> $log_file 
+exit
+
+fi
+
+
+mv $emp_file  $targ_path
+
+status=$?
+
+
+if [ $status -eq 0 ]; then 
+
+echo " file moved succesfully to the destination dir"
+else
+echo "  file is not moved properly" >> $log_file
+exit 
+
+fi
+
+
+if [ -s $targ_file ]; then 
+
+echo "file has the record" >> $log_file
+ 
+else
+echo " file is empty so the script is terminatings" >> $log_file
+exit
+fi
+
+
+
+cut -d "," -f2 $targ_file > $salary_file
+
+status=$?
+
+
+if [ $status -eq 0 ]; then
+echo " salary file has been created ">> $log_file
+
+else
+echo " failure in the cut command" >> $log_file
+exit
+fi
